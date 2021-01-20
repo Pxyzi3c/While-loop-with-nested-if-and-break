771922397080c93ff337bd366800c0fa649a85c6
Pxyzi3c <72428021+Pxyzi3c@users.noreply.github.com>
2021-01-20 18:57:23 +0800

    Update while with nested if.c

--- while with nested if.c
+++ while with nested if.c
@@ -40,32 +40,3 @@ int main()
 getch();
     return 0;
 }
-
-
-
-
-
-
-
-/*int main()
-{   float gsales, tsalary, salary;
-    tsalary=0;
-
-    while(gsales!=-1)
-    {   p("ENTER SALES IN DOLLAR(-1 TO END): ");
-        s("%f",&gsales);
-        if(gsales==-1)
-        {
-            break;
-        }
-        salary=200+(gsales*.09);
-        p("SALARY: $%.2f\n",salary);
-        p("\n");
-        tsalary=tsalary+salary;
-    }
-    p("\nTHE TOTAL SALARY FOR THE WEEK: $%.2f\n",tsalary);
-    p("GOOD DAY! THANK YOU FOR USING THE SYSTEM!");
-getch();
-    return 0;
-}*/
-
