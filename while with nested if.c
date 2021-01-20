38461a0736edd79a7c2b4503ecd733a2f0fe539e
Pxyzi3c <72428021+Pxyzi3c@users.noreply.github.com>
2021-01-20 18:55:55 +0800

    Add files via upload
    Develop a  program that uses while statement to determine the gross pay for each of several employees.  The company pays “straight time” for the first 40 hours 	worked by each employee and pays “time-and-a-half” for all hours worked in excess of 40 hours.  Your program should input this information for each employee and should determine and display the employee’s gross pay.
    
    EXAMPLE RUN:
    ENTER HOURS WORKED(-1 TO END): 39
    ENTER HOURLY RATE OF THE WORKERS($00.00): 10.00
    THE SALARY IS: $390.00
    
    ENTER HOURS WORKED(-1 TO END): 40
    ENTER HOURLY RATE OF THE WORKERS($00.00): 10.00
    THE SALARY IS: $400.00
    
    ENTER HOURS WORKED(-1 TO END): 41
    ENTER HOURLY RATE OF THE WORKERS($00.00): 10.00
    THE SALARY IS: $415.00
    
    ENTER HOURS WORKED(-1 TO END): -1
    
    THE TOTAL SALARY OF ALL EMPLOYEES: $1205.00
    GOOD DAY! THANK YOU FOR USING THE SYSTEM!

---
+++ while with nested if.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define p printf
+#define s scanf
+
+/*HARVY JONES PONTILLAS DICT 1-2*/
+
+int main()
+{   int hrsw;
+    float hrate, salary, tsalary;
+    tsalary=0;
+
+    while(hrsw!=-1)
+    {
+        p("ENTER HOURS WORKED(-1 TO END): ");
+        s("%d",&hrsw);
+        if(hrsw==-1)
+        {
+            break;
+        }
+        else if(hrsw>40)
+        {
+            p("ENTER HOURLY RATE OF THE WORKER($00.00): ");
+            s("%f",&hrate);
+            salary=(hrsw*hrate)+(hrate/2);
+            p("THE SALARY IS: $%.2f", salary);
+        }
+        else if(hrsw<=40)
+        {
+            p("ENTER HOURLY RATE OF THE WORKER($00.00): ");
+            s("%f",&hrate);
+            salary=hrsw*hrate;
+            p("THE SALARY IS: $%.2f", salary);
+        }
+        p("\n\n");
+        tsalary=tsalary+salary;
+    }
+    p("\nTHE TOTAL SALARY OF ALL EMPLOYEES: $%.2f\n",tsalary);
+    p("GOOD DAY! THANK YOU FOR USING THE SYSTEM!");
+getch();
+    return 0;
+}
+
+
+
+
+
+
+
+/*int main()
+{   float gsales, tsalary, salary;
+    tsalary=0;
+
+    while(gsales!=-1)
+    {   p("ENTER SALES IN DOLLAR(-1 TO END): ");
+        s("%f",&gsales);
+        if(gsales==-1)
+        {
+            break;
+        }
+        salary=200+(gsales*.09);
+        p("SALARY: $%.2f\n",salary);
+        p("\n");
+        tsalary=tsalary+salary;
+    }
+    p("\nTHE TOTAL SALARY FOR THE WEEK: $%.2f\n",tsalary);
+    p("GOOD DAY! THANK YOU FOR USING THE SYSTEM!");
+getch();
+    return 0;
+}*/
+
